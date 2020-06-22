c081c7911832f298db6f4d43bd8864b6e3960eee
pathas <pathas@naver.com>
2020-06-23 00:12:36 +0900

    Making Image Carousel

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
+import { PlusOutlined } from "@ant-design/icons";
 
 const PostImages = ({ images }) => {
-  return <div>구현중...</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+  if (images.length === 2) {
+    return (
+      <div>
+        <img
+          src={images[0].src}
+          width="50%"
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          src={images[1].src}
+          width="50%"
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <img
+        src={images[0].src}
+        width="50%"
+        alt={images[0].src}
+        onClick={onZoom}
+      />
+      <div
+        role="presentation"
+        style={{
+          display: "inline-block",
+          width: "50%",
+          textAlign: "center",
+          verticalAlign: "middle",
+        }}
+        onClick={onZoom}
+      >
+        <PlusOutlined />
+        <br />
+        {images.length - 1}개의 사진 더 보기
+      </div>
+    </div>
+  );
 };
 
 PostImages.propTypes = {
